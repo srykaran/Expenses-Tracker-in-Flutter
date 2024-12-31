046e224fcc427841f8ad3ffb7f276f97b1835537
Harishchandra <karanpednekar5055@gmail.com>
2024-12-31 14:40:44 +0530

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+
+  }
+}
+
+
+class _ExpensesState extends State<Expenses>{
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  }
